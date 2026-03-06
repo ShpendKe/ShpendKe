2355f9a52577ff822891abe8012ab9211f9e48be
Shpend Kelmendi <shekel7@hotmail.com>
2026-03-07 00:11:38 +0100

    fix style

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -4,16 +4,18 @@ import type BlogPostItemType from "@theme/BlogPostItem";
 import type { WrapperProps } from "@docusaurus/types";
 import { useBlogPost } from "@docusaurus/plugin-content-blog/client";
 import Comments from "@site/src/components/Comments";
+import CenteredImage from '@site/src/components/Image';
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
-export default function BlogPostItemWrapper(props: Props): JSX.Element {
-  const { metadata, isBlogPostPage } = useBlogPost();
+export default function BlogPostItemWrapper(props: Props): React.JSX.Element {
+  const { metadata, assets, isBlogPostPage } = useBlogPost();
   const { comments = true } = metadata.frontMatter;
 
   return (
-    <>
-      <BlogPostItem {...props} />
+    <>  
+      <CenteredImage src={assets.image} caption="" />
+      <BlogPostItem {...props}  />
       {comments && isBlogPostPage && <Comments />}
     </>
   );
